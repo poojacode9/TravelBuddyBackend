79218a0bb068b849a6342a6167dcda6045ab2d7e
Pooja <poojakchavan02@gmail.com>
2026-07-26 08:46:45 +0530

    Add TourPackage repository

---
+++ src/main/java/com/travel/repository/TourPackageRepository.java
@@ -0,0 +1,20 @@
+package com.travel.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.travel.entity.TourPackage;
+import com.travel.enums.PackageStatus;
+
+@Repository
+public interface TourPackageRepository extends JpaRepository<TourPackage, Long> {
+	
+	List<TourPackage> findByStatus(PackageStatus status);
+
+	List<TourPackage> findByGuideId(Long guideId);
+	
+
+
+}
