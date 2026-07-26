7faef206ff26d292c455835c9bb12748f09f04fc
Pooja <poojakchavan02@gmail.com>
2026-07-26 08:50:37 +0530

    Add Booking repository

---
+++ src/main/java/com/travel/repository/BookingRepository.java
@@ -0,0 +1,16 @@
+package com.travel.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.travel.entity.Booking;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findByUserId(Long userId);
+
+    List<Booking> findByTourPackageGuideId(Long guideId);
+
+    List<Booking> findByTourPackageId(Long packageId);
+}
