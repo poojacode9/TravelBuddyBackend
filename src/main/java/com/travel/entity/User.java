4e3bfabd3bd31f31079171231dbd7bb9d90f7d1f
Pooja <poojakchavan02@gmail.com>
2026-07-26 08:32:32 +0530

    Add User entity

---
+++ src/main/java/com/travel/entity/User.java
@@ -0,0 +1,34 @@
+package com.travel.entity;
+
+import com.travel.enums.Role;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String phone;
+
+    private Role role;
+}
